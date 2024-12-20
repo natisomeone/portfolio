91b13aa10b2f84130d158ee6e34dd8bc7444f695
Nati <natisomeone@gmail.com>
2024-12-20 11:22:59 +0300

    app-component-updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
-import { MenuBtnComponent } from './components/menu-btn/menu-btn.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, MenuBtnComponent],
+  imports: [RouterOutlet],
   template: ` <main class="w-full h-full relative overflow-x-hidden">
-    <!-- <app-menu [isMenuVisible]="isMenuVisible"/> -->
-    <!-- <app-menu-btn [toggleMenu]="toggleVisibility"/> -->
     <router-outlet></router-outlet>
   </main>`,
 })
 export class AppComponent {
   title = 'portfolio';
 
-  isMenuVisible = false
-
-  toggleVisibility($event?: Event): void {
-    this.isMenuVisible = !this.isMenuVisible
-    $event?.stopPropagation()
-  }
 }
