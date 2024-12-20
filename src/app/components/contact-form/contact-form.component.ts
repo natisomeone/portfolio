a2fbfedb2344c0c57778f3672af45fe0c827a510
Nati <natisomeone@gmail.com>
2024-12-20 10:50:29 +0300

    minor-updates

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../ui/button/button.component";
+import { ButtonComponent } from '../ui/button/button.component';
 
 @Component({
   selector: 'app-contact-form',
@@ -22,6 +22,19 @@ import { ButtonComponent } from "../ui/button/button.component";
           </div>
         </div>
       }
+      <div class="w-full flex border-b border-white/10 mb-16">
+          <p class="text-white/50 text-xs mt-1 font-medium">05</p>
+          <div class="ml-8 flex-1 flex flex-col">
+            <label for="name" class="text-white/90 focus:text-white/50 text-xl tracking-tight"
+              >Your Message</label
+            >
+            <textarea
+              type="text"
+              placeholder="Hello Nate, I would love to collaborate with you to help me with..."
+              class="outline-none w-full border-none placeholder-white/40 text-white/95 bg-transparent mt-4 pr-2 min-h-32 text-xl"
+            ></textarea>
+          </div>
+        </div>
       <app-button [label]="'Send it!'" [textColor]="'text-white'" [borderColor]="'border-white/10'"/>
     </form>
   `,
@@ -53,5 +66,5 @@ export class ContactFormComponent {
       placeholder: 'Web Design, Web Development, Re-design...',
       type: 'text',
     },
-  ]
+  ];
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -9,7 +9,7 @@ import { ContactFormComponent } from '../../components/contact-form/contact-form
   template: `
     <div class="w-full h-auto relative bg-surface-dark">
       <app-navbar [textColor]="'text-white/70'" />
-      <div class="w-full pl-32">
+      <div class="w-full pl-24">
         <h2
           class="text-8xl text-white/95 tracking-tighter font-medium ml-4 mt-20"
         >
@@ -23,25 +23,40 @@ import { ContactFormComponent } from '../../components/contact-form/contact-form
 
           <!-- Quick Contacts -->
           <div class="w-full md:pl-32">
+            <!-- CONTACT-DETAILS -->
             <div class="mb-12">
               <h4 class="text-[10px] mb-4 uppercase font-medium text-white/40">
                 Contact details
               </h4>
               <ul>
                 @for (detail of details; track $index) {
-                <li class="mb-1 font-medium text-white/90 tracking-tight">
+                <li class="mb-2 font-medium text-white/90 tracking-tight">
                   <a [href]="detail.link">{{ detail.label }}</a>
                 </li>
                 }
               </ul>
             </div>
+            <!-- BUSINESS INFORMATION -->
+            <div class="mb-12">
+              <h4 class="text-[10px] mb-4 uppercase font-medium text-white/40">
+                Work conditions
+              </h4>
+              <ul>
+                @for (work_condition of work_conditions; track $index) {
+                <li class="mb-2 font-medium text-white/90 tracking-tight">
+                  <p>{{ work_condition.description }}</p>
+                </li>
+                }
+              </ul>
+            </div>
+            <!-- SOCIALS -->
             <div>
               <h4 class="text-[10px] uppercase font-medium text-white/40 mb-4">
                 Socials
               </h4>
               <ul>
                 @for (social of socials; track $index) {
-                <li class="mb-1 font-medium text-white/90 tracking-tight">
+                <li class="mb-2 font-medium text-white/90 tracking-tight">
                   <a [href]="social.link">{{ social.label }}</a>
                 </li>
                 }
@@ -65,6 +80,21 @@ export class ContactComponent {
       link: '',
     },
   ];
+
+  work_conditions = [
+    {
+      icon: '',
+      description: 'I am available between 3:00AM - 10:00PM'
+    },
+    {
+      icon: '',
+      description: 'I work remotely from anywhere.'
+    },
+    {
+      icon: '',
+      description: 'Preferable contact methods - Email / Instagram'
+    },
+  ]
 
   socials = [
     {
